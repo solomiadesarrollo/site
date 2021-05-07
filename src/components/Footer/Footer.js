e9e446cf71b3bd385fb5dff2d3b26179ca7776cb
Naharca <naharacastillo720@gmail.com>
2021-05-07 15:08:25 -0300

    footer adapt browser tab icon

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -10,34 +10,26 @@ function Footer() {
     return (
         <footer className="footer">
             <div className="footer__element">
-                <h2>Ayuda</h2>
-                <div>
-                    <Link className="footer__link" to="/"><p className="footer__paragraph">Cómo comprar</p></Link>
-                    <Link className="footer__link" to="/"><p className="footer__paragraph">Cambios y Devoluciones</p></Link>
-                </div>
-            </div>
-            <div className="footer__element">
-                <h2>Nosotras</h2>
+                <h2>Info</h2>
                 <div>
                     <Link className="footer__link" to={{                             
                         pathname: '/',                    
                         state: {                        
                         toAboutUs: true                    
                         }                
-                    }}><p className="footer__paragraph">Quiénes somos</p></Link>
+                    }}><p className="footer__paragraph">Nosotras</p></Link>
                     <Link className="footer__link" to={{                             
                         pathname: '/',                    
                         state: {                        
                         toCategories: true                    
                         }                
-                    }}><p className="footer__paragraph">Catálogo</p></Link>
-                    <Link className="footer__link" to="/"><p className="footer__paragraph">Contacto</p></Link>
+                    }}><p className="footer__paragraph">Colección</p></Link>
+                    <Link className="footer__link" to="/"><p className="footer__paragraph">¿Cómo comprar?</p></Link>
                 </div>
             </div>
             <div className="footer__element">
                 <h2>Seguinos</h2>
                 <div className="footer__elementBody footer__elementBody--redes">
-                    <Link className="footer__link footer__link--redes nav__item" to="/"><img className="footer__iconosRedes" src={whatsapp_icon} alt="phone"></img><p className="footer__paragraph">WhatsApp</p></Link>
                     <Link className="footer__link footer__link--redes nav__item" to="/"><img className="footer__iconosRedes" src={ig_icon} alt="instagram"></img><p className="footer__paragraph">Instagram</p></Link>
                     <Link className="footer__link footer__link--redes nav__item" to="/"><img className="footer__iconosRedes" src={facebook_icon} alt="facebook"></img><p className="footer__paragraph">Facebook</p></Link>
                 </div>
